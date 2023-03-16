5445f49f260d23a0239c8bcb98f169cfbc9d6917
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-03-15 20:57:58 -0400

    tracking prevState while using setTimeout

--- src/tutorial/01-useState/UseStateGotcha1.jsx
+++ src/tutorial/01-useState/UseStateGotcha1.jsx
@@ -17,12 +17,36 @@ const UseStateGotcha1 = () => {
     });
   };
 
+  //
+  // const handleClickWithTimeout = () => {
+  //   setTimeout(() => {
+  //     console.log('clicked');
+  //     setGotcha(gotcha + 1);
+  //   }, 3000);
+  // };  result will still be 1 with multple clicks, because state isn't updated immediately, it is referencing initial state everytime you click
+
+  // will work because we are passing a function to setState that receives the previous state as an argument and returns the new state
+  const handleClickWithTimeout = () => {
+    setTimeout(() => {
+      console.log('clicked');
+      return setGotcha((currentState) => {
+        const newState = currentState + 1;
+        console.log(newState);
+        return newState;
+      });
+    }, 3000);
+  };
+
   return (
     <>
       <h1>UseState Gotcha 1</h1>
       <h2>{gotcha}</h2>
       <button onClick={gotchaHandler} className='btn'>
         gotcha plus one
+      </button>
+      <br />
+      <button onClick={handleClickWithTimeout} className='btn'>
+        set timeout
       </button>
     </>
   );
