8acd8e3e57a43c2001c1f75eebca50974844b75c
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-03-17 17:14:13 -0400

    multiple useEffects

--- src/tutorial/02-useEffect/03-multiple-effects.jsx
+++ src/tutorial/02-useEffect/03-multiple-effects.jsx
@@ -1,1 +1,41 @@
+import { useState, useEffect } from 'react';
 
+const MultipleEffects = () => {
+  const [value, setValue] = useState(0);
+  const [secondValue, setSecondValue] = useState(0);
+
+  useEffect(() => {
+    console.log('hello from first useEffect');
+  }, [value]);
+
+  useEffect(() => {
+    console.log('hello from second useEffect');
+  }, [secondValue]);
+  return (
+    <div>
+      <h1>value : {value}</h1>
+      <button className='btn' onClick={() => setValue(value + 1)}>
+        value
+      </button>
+      <button
+        className='btn'
+        onClick={() => setValue(0)}
+        style={{ backgroundColor: 'red' }}
+      >
+        reset
+      </button>
+      <h1>second value : {secondValue}</h1>
+      <button className='btn' onClick={() => setSecondValue(secondValue + 1)}>
+        second value
+      </button>
+      <button
+        className='btn'
+        onClick={() => setSecondValue(0)}
+        style={{ backgroundColor: 'red' }}
+      >
+        reset
+      </button>
+    </div>
+  );
+};
+export default MultipleEffects;
