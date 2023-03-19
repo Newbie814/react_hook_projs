87247d3da97e8fcce4807fb0686b376375ea4329
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-03-19 17:37:58 -0400

    conditional rendering basics and setup toggle

--- src/tutorial/03-conditional-rendering/01-multiple-returns-basics.jsx
+++ src/tutorial/03-conditional-rendering/01-multiple-returns-basics.jsx
@@ -1,6 +1,37 @@
 import { useEffect, useState } from 'react';
 
+// simple toggle rendering:
+
+// const MultipleReturnsBasics = () => {
+//   const [isLoading, setIsLoading] = useState(false);
+//   const notLoading = <h2>Multiple Returns Basics</h2>;
+//   const loading = <h2>Loading...</h2>;
+//   return (
+//     <div>
+//       {isLoading ? loading : notLoading}
+//       <button type='button' onClick={() => setIsLoading(!isLoading)}>
+//         change
+//       </button>
+//     </div>
+//   );
+// };
+
 const MultipleReturnsBasics = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
   return <h2>Multiple Returns Basics</h2>;
 };
 export default MultipleReturnsBasics;
