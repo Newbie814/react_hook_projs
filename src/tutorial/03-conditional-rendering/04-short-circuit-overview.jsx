19ec2317f45e7751086f1f796bb54eaba5023a22
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-03-19 21:33:42 -0400

    added an example with storing the logic in a variable

--- src/tutorial/03-conditional-rendering/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/04-short-circuit-overview.jsx
@@ -6,6 +6,11 @@ const ShortCircuitOverview = () => {
   // truthy
   const [name, setName] = useState('Matthew');
 
+  // for below, you can also store the logic in a variable and then just declare the variable in the JSX, for example:
+  // const firstValue = text || 'Hello World';
+  // const secondValue = text && 'Hello World';
+  // and then in the JSX, you can just do:
+  // <h4>Falsy OR : {firstValue}</h4>
   return (
     <>
       <div>
@@ -18,6 +23,7 @@ const ShortCircuitOverview = () => {
       <h4>Truthy OR : {name || 'Hello World'}</h4>
       {/* // returns first value, because first value is truthy */}
       <h4>Truthy AND : {name && 'Hello World'}</h4>
+      {/* // returns second value, because first value is truthy */}
     </>
   );
 };
