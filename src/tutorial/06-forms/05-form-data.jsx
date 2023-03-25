db031c5b819dffc26f84305a4e183e73895a8e0e
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-03-24 22:16:46 -0400

    form data api, Object.fromEntries

--- src/tutorial/06-forms/05-form-data.jsx
+++ src/tutorial/06-forms/05-form-data.jsx
@@ -5,6 +5,13 @@ const UncontrolledInputs = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    // console.log(formData);
+    // const email = formData.get('email');
+    // console.log(email);
+    const newUser = Object.fromEntries(formData);
+    console.log(newUser);
+    e.currentTarget.reset();
   };
   return (
     <div>
@@ -24,7 +31,7 @@ const UncontrolledInputs = () => {
           </label>
           <input type='email' className='form-input' id='email' name='email' />
         </div>
-        {/* email */}
+        {/* password */}
         <div className='form-row'>
           <label htmlFor='password' className='form-label'>
             Password
