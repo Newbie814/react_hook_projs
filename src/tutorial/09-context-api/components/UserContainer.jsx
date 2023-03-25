ed3b3b2469c689e15d26f18fff4086cc4d8fa0cc
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-03-25 16:26:58 -0400

    fixed conditional rendering bug

--- src/tutorial/09-context-api/components/UserContainer.jsx
+++ src/tutorial/09-context-api/components/UserContainer.jsx
@@ -2,8 +2,15 @@ const UserContainer = ({ name, logout }) => {
   return (
     <>
       <h2>UserContainer</h2>
-      <h3>{name.name}</h3>
-      {name ? logout() : <h3>please login</h3>}
+
+      {name ? (
+        <>
+          <h3>{name.name}</h3>
+          {logout()}
+        </>
+      ) : (
+        <h3>please login</h3>
+      )}
     </>
   );
 };
