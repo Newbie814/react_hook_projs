ccbf832e492cf1c834228085d8f065556749f4bd
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-03-26 19:03:47 -0400

    setup actions as variables and moved into separate module

--- src/tutorial/10-useReducer/01-userReducer.jsx
+++ src/tutorial/10-useReducer/01-userReducer.jsx
@@ -1,18 +1,20 @@
 import { useReducer } from 'react';
 import { data } from '../../data';
 
+import { CLEAR_LIST, RESET, REMOVE_ITEM } from './actions';
+
 const defaultState = {
   people: data,
 };
 
 const reducer = (state, action) => {
-  if (action.type === 'CLEAR_LIST') {
+  if (action.type === CLEAR_LIST) {
     return { ...state, people: [] };
   }
-  if (action.type === 'RESET') {
+  if (action.type === RESET) {
     return { ...state, people: data };
   }
-  if (action.type === 'REMOVE_ITEM') {
+  if (action.type === REMOVE_ITEM) {
     return {
       ...state,
       people: state.people.filter((person) => person.id !== action.payload),
---
+++ src/tutorial/10-useReducer/actions.js
@@ -0,0 +1,5 @@
+const CLEAR_LIST = 'CLEAR_LIST';
+const RESET = 'RESET';
+const REMOVE_ITEM = 'REMOVE_ITEM';
+
+export { CLEAR_LIST, RESET, REMOVE_ITEM };
